c6643cae62f9a83b9f01a1a40cb6f26893f99e2d
httung190597 <tung190597@gmail.com>
2020-10-05 19:04:34 +0700

    Updates

---
+++ precache-manifest.5c656330de9ef7e13ab41922833ccdcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1d58f621a510e1f872faa68986e68e4",
+    "url": "/test-fresher/index.html"
+  },
+  {
+    "revision": "10dd8bfc67e0ee6a67a3",
+    "url": "/test-fresher/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0aa8bf3c619c49c04082",
+    "url": "/test-fresher/static/js/2.65bdb4ee.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/test-fresher/static/js/2.65bdb4ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10dd8bfc67e0ee6a67a3",
+    "url": "/test-fresher/static/js/main.1e468b7d.chunk.js"
+  },
+  {
+    "revision": "0030a489a29ab8d98909",
+    "url": "/test-fresher/static/js/runtime-main.4b62231d.js"
+  }
+]);
